682f28eb03a17992413e652aae78112e2676361b
Alejandro <alejandrofigueirasx@gmail.com>
2023-11-20 23:19:40 -0500

    Agrega uso de dos estados dependientes

--- prueba-tecnica-gato/src/App.jsx
+++ prueba-tecnica-gato/src/App.jsx
@@ -1,9 +1,31 @@
-function App () {
+import { useEffect, useState } from "react"
+
+const App = () => {
   
+  const [fact, setFact] = useState(null)
+  const [imageUrl, setImageUrl] = useState(null)
+
+  useEffect(() => {
+    fetch('https://catfact.ninja/fact')
+      .then(res => res.json())
+      .then(data => {
+        const fact = data.fact;
+        setFact(fact)
+        const initialWords = fact.split(" ").slice(0,3).join(" ");
+
+        fetch(`https://cataas.com/cat/says/${initialWords}?size=50&json=true`)
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+            setImageUrl(`https://cataas.com/cat/says/${initialWords}`)
+          })
+      })
+  }, [])
 
   return (
     <main>
-      
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img alt={`Image extracted using three first three words for ${fact}`} src={imageUrl} />}
     </main>
   )
 }
