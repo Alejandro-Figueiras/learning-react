562a07e003787eb3b1bcc397ff511450ff0cf8c5
Alejandro <alejandrofigueirasx@gmail.com>
2023-12-07 11:51:59 -0500

    Evita rebusqueda

--- buscador-peliculas/src/hooks/useMovies.js
+++ buscador-peliculas/src/hooks/useMovies.js
@@ -1,15 +1,19 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { fetchMovies } from "../services/movies"
 
 export function useMovies() {
   const [movies, setMovies ] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const busquedaAnterior = useRef('');
 
   const getMovies = async(search) => {
+    if (search == busquedaAnterior.current) return
+
     try {
       setLoading(true)
       setError(null);
+      busquedaAnterior.current = search
       if (search) {
         setMovies(await fetchMovies(search))
       } else {
